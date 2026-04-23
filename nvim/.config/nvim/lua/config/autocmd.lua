831acc38d4d666ef2084e381b0d3d894fcb0b729
lanre-arch <owolabilanre@gmail.com>
2026-04-23 19:40:15 -0400

    added a few color theme to nvim

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -1,123 +1,163 @@
---useful stuff
+-- =========================
+-- Autocommands (cleaned up)
+-- =========================
 
--- close nvim-tree if it's last buffer open
+-- Create reusable augroups
+local general = vim.api.nvim_create_augroup("general", { clear = true })
+local number_toggle = vim.api.nvim_create_augroup("number_toggle", { clear = true })
+local alpha_group = vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+
+-- -------------------------
+-- Close NvimTree if it's the last window
+-- -------------------------
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function()
-		if #vim.api.nvim_list_bufs() == 1 and vim.bo.filetype == "NvimTree" then
-	vim.cmd("quit")
-	end
-	end,
+  group = general,
+  callback = function()
+    local wins = vim.api.nvim_list_wins()
+    if #wins == 1 then
+      local buf = vim.api.nvim_win_get_buf(wins[1])
+      if vim.bo[buf].filetype == "NvimTree" then
+        vim.cmd("quit")
+      end
+    end
+  end,
 })
 
+-- -------------------------
+-- Auto-create missing dirs on save
+-- -------------------------
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = general,
+  callback = function()
+    local file = vim.api.nvim_buf_get_name(0)
+    if file == "" then return end
 
--- auto-create missing dirs when saving a file
---vim.api.nvim_create_autocmd("BufWritePre", {
---	pattern = "*",
---	callback = function()
---	local dir = vim.fn.expand("<afile>:p:h")
---	if vim.fn.isdirectory(dir) == 0 then
---		vim.fn.mkdir(dir, "p")
---	end
---end,
---})
+    local dir = vim.fn.fnamemodify(file, ":p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
 
-
--- linting when file is written to
+-- -------------------------
+-- Lint on save
+-- -------------------------
 vim.api.nvim_create_autocmd("BufWritePost", {
+  group = general,
   callback = function()
-    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-    -- for the current filetype, on write
-    require("lint").try_lint()
+    local ok, lint = pcall(require, "lint")
+    if ok then
+      lint.try_lint()
+    end
   end,
 })
 
-
--- spellcheck in md
+-- -------------------------
+-- Markdown settings
+-- -------------------------
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	command = "setlocal spell wrap",
+  group = general,
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.wrap = true
+  end,
 })
 
-
--- disable automatic comment on newline
+-- -------------------------
+-- Disable auto comment on newline
+-- -------------------------
 vim.api.nvim_create_autocmd("FileType", {
-		pattern = "*",
-		callback = function()
-		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-		end,
+  group = general,
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
 })
 
-
--- highlight text on yank
+-- -------------------------
+-- Highlight on yank
+-- -------------------------
 vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-	vim.highlight.on_yank({ timeout = 300 })
-	end,
-})
-
-
--- reload files on external change
-vim.api.nvim_create_autocmd("FocusGained", {
-	pattern = "*",
-	command = "checktime",
-})
-
-
--- restore cursor pos on file open
-vim.api.nvim_create_autocmd("BufReadPost", {
-	pattern = "*",
-	callback = function()
-	local line = vim.fn.line("'\"")
-	if line > 1 and line <= vim.fn.line("$") then
-		vim.cmd("normal! g'\"")
-	end
-end,
-})
-
-vim.api.nvim_create_autocmd("VimEnter", {
+  group = general,
   callback = function()
-    local startuptime = vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time))
-    vim.g.startup_time_ms = string.format("%.2f ms", startuptime * 1000)
+    vim.highlight.on_yank({ timeout = 300 })
   end,
 })
 
-vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+-- -------------------------
+-- Reload file if changed externally
+-- -------------------------
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  group = general,
+  callback = function()
+    vim.cmd("checktime")
+  end,
+})
+
+-- -------------------------
+-- Restore cursor position
+-- -------------------------
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = general,
+  callback = function()
+    local line = vim.fn.line("'\"")
+    if line > 1 and line <= vim.fn.line("$") then
+      vim.cmd("normal! g'\"")
+    end
+  end,
+})
+
+-- -------------------------
+-- Startup time tracking
+-- -------------------------
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = general,
+  callback = function()
+    if vim.g.start_time then
+      local startuptime = vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time))
+      vim.g.startup_time_ms = string.format("%.2f ms", startuptime * 1000)
+    end
+  end,
+})
+
+-- -------------------------
+-- Alpha dashboard reopen logic
+-- -------------------------
 vim.api.nvim_create_autocmd("User", {
-	pattern = "BDeletePre *",
-	group = "alpha_on_empty",
-	callback = function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local name = vim.api.nvim_buf_get_name(bufnr)
-		if name == "" then
+  group = alpha_group,
+  pattern = "BDeletePre *",
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if name == "" then
       vim.cmd([[:Alpha | bd#]])
-		end
-	end,
+    end
+  end,
 })
 
-
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
-         vim.opt.relativenumber = true
+-- -------------------------
+-- Relative number toggle (smart)
+-- -------------------------
+vim.api.nvim_create_autocmd(
+  { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+  {
+    group = number_toggle,
+    callback = function()
+      if vim.o.number and vim.api.nvim_get_mode().mode ~= "i" then
+        vim.opt.relativenumber = true
       end
-   end,
-})
+    end,
+  }
+)
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu then
-         vim.opt.relativenumber = false
-         -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
-         -- Workaround for https://github.com/neovim/neovim/issues/32068
-         if not vim.tbl_contains({"@", "-"}, vim.v.event.cmdtype) then
-            vim.cmd "redraw"
-         end
+vim.api.nvim_create_autocmd(
+  { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+  {
+    group = number_toggle,
+    callback = function()
+      if vim.o.number then
+        vim.opt.relativenumber = false
       end
-   end,
-})
+    end,
+  }
+)
