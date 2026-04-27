d5457ea5e813f5187008c2d6568d301b1c43bc0c
Lanre <owolabilanre2006@gmail.com>
2026-04-27 23:39:28 +0100

    updating dots repo

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "black" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    markdown = { "prettier" },
+  },
+
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
