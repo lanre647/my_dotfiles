29758029764adac276b9cb11dfd750bdb8a54644
Lanre <owolabilanre2006@gmail.com>
2026-05-01 17:29:18 +0100

    added c/c++ lsp's

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,19 +1,26 @@
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" },
-    html = { "prettier" },
-    css = { "prettier" },
-    markdown = { "prettier" },
-  },
+local is_termux = vim.env.PREFIX and vim.env.PREFIX:match("termux")
 
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		-- Add C and C++ here
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		json = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+		markdown = { "prettier" },
+	},
+
+	format_on_save = {
+		-- On Termux, you might want a slightly longer timeout
+		-- as the CPU on the Itel can sometimes take a second to process
+		timeout_ms = is_termux and 1000 or 500,
+		lsp_fallback = true,
+	},
 })
